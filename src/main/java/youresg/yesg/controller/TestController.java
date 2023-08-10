6bb73139571ebdfe750015ea5d39478dedff6d35
suddiyo <suddii01@sju.ac.kr>
2023-08-11 02:20:48 +0900

    Test: Get, Post API 요청 테스트

--- src/main/java/youresg/yesg/controller/TestController.java
+++ src/main/java/youresg/yesg/controller/TestController.java
@@ -4,15 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @Slf4j
 public class TestController {
 
@@ -24,7 +20,7 @@ public class TestController {
     @PostMapping ("/success")
     public ResponseEntity postSuccessMessage(@RequestParam String param){
         log.info("param = {}", param);
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        return new ResponseEntity<>("param", HttpStatus.OK);
     }
 
 }
