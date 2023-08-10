0d949529f6663190be43e017f77c583e2d8f2fa3
suddiyo <suddii01@sju.ac.kr>
2023-08-10 23:17:29 +0900

    initial commit

---
+++ src/main/java/youresg/yesg/YesgApplication.java
@@ -0,0 +1,13 @@
+package youresg.yesg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/youresg/yesg/YesgApplicationTests.java
@@ -0,0 +1,13 @@
+package youresg.yesg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
