94020bf4924d61b6929b6b71720f1245c3ae2406
nyeroni <nir2y@naver.com>
2023-08-31 17:23:14 +0900

    Update: BoardRepository 수정

--- src/main/java/youresg/yesg/domain/board/BoardRepository.java
+++ src/main/java/youresg/yesg/domain/board/BoardRepository.java
@@ -1,6 +1,20 @@
 package youresg.yesg.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("SELECT b FROM Board b ORDER BY b.id DESC")
+    List<Board> findAllDesc();
+
+    @Query("SELECT b FROM Board b where b.title LIKE %:keyword%")
+    List<Board> findBySearch(String keyword);
+
+    @Query("SELECT DISTINCT b FROM Board b JOIN b.boardHashtagList h WHERE h.hashtag = :tagName")
+    List<Board> findByHashtag(String tagName);
 }
